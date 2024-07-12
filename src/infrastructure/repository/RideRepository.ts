5a5f8340cfe2d4ae47851323a64197c6ac661b48
luccas-specht <luccasspecht70@gmail.com>
2024-07-12 15:35:13 -0300

    test: add integration test

--- src/infrastructure/repository/RideRepository.ts
+++ src/infrastructure/repository/RideRepository.ts
@@ -6,11 +6,48 @@ export class RideRepositoryDatabase implements RideRepository {
   constructor(readonly connection: DatabaseConnection) {}
 
   async getRideByPassengerId(passengerId: string): Promise<Ride | undefined> {
-    throw new Error('Method not implemented.');
+    const [rideData] = await this.connection.query(
+      'select * from cccat17.ride where passenger_id = $1',
+      [passengerId]
+    );
+
+    if (rideData) {
+      return new Ride(
+        rideData.ride_id,
+        rideData.passenger_id,
+        rideData.driver_id,
+        rideData.status,
+        rideData.fare,
+        rideData.distance,
+        rideData.from_lat,
+        rideData.from_long,
+        rideData.to_lat,
+        rideData.to_long,
+        rideData.date
+      );
+    }
+
+    return undefined;
   }
 
   async requestRide(ride: Ride): Promise<void> {
-    throw new Error('Method not implemented.');
+    console.log({ ride });
+    await this.connection.query(
+      'insert into cccat17.ride (ride_id, passenger_id, driver_id, status, fare, distance, from_lat, from_long, to_lat, to_long, date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)',
+      [
+        ride.rideId,
+        ride.passengerId,
+        ride.driverId,
+        ride.status,
+        ride.fare,
+        ride.distance,
+        ride.fromLat,
+        ride.fromLong,
+        ride.toLat,
+        ride.toLong,
+        ride.date,
+      ]
+    );
   }
 }
 
--- test/integration/api.test.ts
+++ test/integration/api.test.ts
@@ -43,7 +43,40 @@ test('Não deve criar uma conta de passageiro com cpf inválido pela api', async
 });
 
 test('Deve solicitar uma corrida pela API corretamente', async function () {
-  const responseSignup = await axios.post('http://localhost:3000/request', {});
+  const signup = {
+    name: 'John Doe lukinhas',
+    email: `john.doe${Math.random()}@gmail.com`,
+    cpf: '97456321558',
+    isPassenger: true,
+  };
+
+  const responseSignup = await axios.post(
+    'http://localhost:3000/signup',
+    signup
+  );
+
   const outputSignup = responseSignup.data;
-  expect(outputSignup.rideId).toBeDefined();
+
+  const input = {
+    passengerId: outputSignup.accountId,
+    fromLat: '-29.8464418',
+    fromLong: '-29.8464418',
+    toLat: '-29.9069906',
+    toLong: '-51.1720954',
+  };
+
+  console.log({ xxxxx: outputSignup.accountId });
+
+  const responseRequestRide = await axios.post(
+    'http://localhost:3000/request',
+    input
+  );
+
+  expect(outputSignup).toBeDefined();
+  expect(outputSignup.accountId).toBeDefined();
+
+  const outputRequestRide = responseRequestRide.data;
+  console.log(outputRequestRide);
+  expect(outputRequestRide).toBeDefined();
+  expect(outputRequestRide.rideId).toBeDefined();
 });
